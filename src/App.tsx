7af5e8804f0c8efb88ba6533e3b5e161384e5e95
topefabusuyi <topefabusuyi3@gmail.com>
2025-07-17 22:14:52 +0100

    frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-black text-white h-screen flex items-center justify-center">
+        welcome to my app frontend
+       </div>
+    </>
+  )
+}
+
+export default App
